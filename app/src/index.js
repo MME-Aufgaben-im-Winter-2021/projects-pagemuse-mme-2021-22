b81e6b1424df9acf143e9352d142824217638280
Johanna Kalinin <91871188+JohannaBaenana@users.noreply.github.com>
2022-03-13 15:30:53 +0100

    Appwritefunktionalität

--- app/src/index.js
+++ app/src/index.js
@@ -108,3 +108,27 @@ function renderCurrentPage() {
 }
 
 init();
+
+// Init your Web SDK
+const appwrite = new Appwrite();
+
+appwrite
+    .setEndpoint('https://appwrite.software-engineering.education/v1') // Your Appwrite Endpoint
+    .setProject('6206643994b46f11896b'); // Your project ID
+
+// Register User
+appwrite
+    .account.create('unique()', 'me2@example.com', 'password', 'Hansi')
+        .then(response => {
+            console.log(response);
+        }, error => {
+            console.log(error);
+        });
+
+// Subscribe to files channel
+appwrite.subscribe('files', response => {
+    if(response.event === 'storage.files.create') {
+        // Log when a new file is uploaded
+        console.log(response.payload);
+    }
+});

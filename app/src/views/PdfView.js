7e5fd17a3eafde54f9a490b03098ea48480820a4
JohannaBaenana <91871188+JohannaBaenana@users.noreply.github.com>
2022-03-26 14:37:37 +0100

    Interaktion zwischen den einzelnen Ansichten

---
+++ app/src/views/PdfView.js
@@ -0,0 +1,6 @@
+//PDF-Funktion und Kommentarfunktion aus Index.js müssen hier rein und folgende Funktionen beinhalten:
+/**
+ * Hier ist ein Bug und zwar, sobald man eine PDF hochlädt, wird diese nur angezeigt, wenn man rein/rauszoomt mit dem Toogle
+ * Das vorgegebene Fenster im UI ist vielleicht zu klein/man sieht die Hälfte nicht, um es später annotieren zu können? Diese zugehörige Canvas eventuell größer machen?
+ * Die Größe der PDF eventuell mit Mausregler/Trackpad rein und rauszoomen und die Datei mit der Maus hin/herschieben
+ */
---
+++ app/src/views/SwitchView.js
@@ -0,0 +1,4 @@
+/**
+ * Der Wechsel zwischen Dokumentenansicht und Schreibtischansicht muss auch hierher ausgelagert werden
+ * Code dafür ist in der Index-Datei
+ */

7e195e95a04d81f2204a9d728a1019856fbf4c84
paulawiesner <paula.wiesner@stud.uni-regensburg.de>
2022-04-07 13:43:49 +0200

    Obere Nav unvollständig

---
+++ app/js/nav.js
@@ -0,0 +1,118 @@
+// https://www.freecodecamp.org/news/reusable-html-components-how-to-reuse-a-header-and-footer-on-a-website/
+import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+import AppwriteService from "./appwriteService.js";
+
+class Nav extends HTMLElement {
+    tooltips = [];
+
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        this.innerHTML = `
+
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+              <!-- Container wrapper -->
+              <div class="container-fluid">
+                <a class="navbar-brand" href="#">
+                  <img src="./app/img/pagemuse.png" alt="" width="100" height="100">
+                </a>
+                
+                <!-- Toggle button -->
+                <button class="navbar-toggler" type="button" data-mdb-toggle="collapse" data-mdb-target="#navbarSupportedContent"
+                  aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                  <i class="fas fa-bars"></i>
+                </button>
+          
+                <!-- Collapsible wrapper -->
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                  <!-- Left links -->
+                  <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li id="btn_documentView" class="nav-item">
+                      <a href="/home" class="nav-link py-3 px-4" aria-current="page" title="Dokumentenansicht"
+                        data-bs-toggle="tooltip" data-bs-placement="right" id="nav-home">
+                        Dokumente
+                      </a>
+                    </li>
+                    <li id="btn_deskView" class="nav-item">
+                      <a href="/pdfViewer" class="nav-link py-3 px-4" title="Schreibtischansicht" data-bs-toggle="tooltip"
+                        data-bs-placement="right" id="nav-pdfViewer">
+                        Schreibtisch
+                      </a>
+                    </li>
+                    <li id="btn_teams" class="nav-item">
+                     <a href="/teams" class="nav-link py-3 px-4" title="Teams" data-bs-toggle="tooltip"
+                        data-bs-placement="right" id="nav-teams">
+                        Teams
+                     </a>
+                    </li>
+                  </ul>
+                  <!-- Left links -->
+                </div>
+                <!-- Collapsible wrapper -->
+          
+              </div>
+              <ul>
+                  <div class="dropdown border-top">
+                      <a href="#" class="d-flex align-items-center p-3 link-dark text-decoration-none dropdown-toggle"
+                          id="dropdownUser3" data-bs-toggle="dropdown" aria-expanded="false">
+                          <img src="https://github.com/mdo.png" alt="mdo" width="24" height="24" class="rounded-circle">
+                      </a>
+                      <ul class="dropdown-menu text-small shadow" aria-labelledby="dropdownUser3">
+                          <li><span class="dropdown-text border-bottom fw-bolder" id="username-dropdown">Username</span></li>
+                          <li><a class="dropdown-item" href="#">Settings</a></li>
+                          <li><a class="dropdown-item" href="/profile">Profile</a></li>
+                          <li>
+                              <hr class="dropdown-divider">
+                          </li>
+                          <li><a class="dropdown-item" id="signout-dropdown" href="#">Sign out</a></li>
+                      </ul>
+                  </div>
+              </ul>
+              <!-- Container wrapper -->
+            </nav>
+            
+        `;
+
+        // activate tooltips
+        var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+        tooltipTriggerList.forEach((tooltipTriggerEl) => {
+            this.tooltips.push(new bootstrap.Tooltip(tooltipTriggerEl))
+        });
+
+        // add classes to elements
+        document.getElementsByTagName("body")[0].classList.add("content", "content-pd");
+        document.getElementById("nav-" + window.location.href.split("/").pop())?.classList.add("active");
+
+        // add event listeners
+        document.getElementById("nav-toggle").addEventListener("click", () => this.toggleNavCollapse(this.tooltips));
+        document.getElementById("signout-dropdown").addEventListener("click", AppwriteService.logout);
+
+        AppwriteService.getAccount().then(res => {
+            document.getElementById("username-dropdown").innerHTML = res.name;
+        });
+    }
+
+    toggleNavCollapse(tooltips) {
+        document.getElementById("sidenav").classList.toggle("nav-collapse");
+        document.getElementsByTagName("body")[0].classList.toggle("content-pd-extended");
+    
+        tooltips.forEach((tooltip) => {
+            tooltip.toggleEnabled();
+        });
+    
+        let toggle = document.getElementById("nav-toggle-icon");
+        let toggle_aria = document.getElementById("nav-toggle-aria");
+        if (toggle.getAttribute("href") === "app/img/bootstrap-icons.svg#chevron-left") {
+            toggle.setAttribute("href", "app/img/bootstrap-icons.svg#chevron-right");
+            toggle_aria.setAttribute("aria-label", "Collapse");
+        }
+        else {
+            toggle.setAttribute("href", "app/img/bootstrap-icons.svg#chevron-left");
+            toggle_aria.setAttribute("aria-label", "Expand");
+        }
+    }
+}
+
+customElements.define('nav-component', Nav);
